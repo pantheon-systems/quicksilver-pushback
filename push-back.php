63c8ce1daf02565eca404efd88f7fd490e81de9d
Kevin Porras <kevin.porras@pantheon.io>
2022-10-26 12:04:40 -0600

    Load other files.

--- push-back.php
+++ push-back.php
@@ -3,6 +3,10 @@
 include __DIR__ . '/http-build-url.php';
 
 include __DIR__ . '/Pushback.php';
+include __DIR__ . '/SecretsBase.php';
+include __DIR__ . '/SecretsInterface.php';
+include __DIR__ . '/SecretsLegacy.php';
+
 
 $pushback = new Pantheon\QuicksilverPushback\Pushback();
 $pushback->pushback('legacy');
