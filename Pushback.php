831dc8245becc02dae93473d296f07419448a199
Kevin Porras <kevin.porras@pantheon.io>
2022-10-26 16:01:07 -0600

    Attempt.

--- Pushback.php
+++ Pushback.php
@@ -274,20 +274,16 @@ class Pushback {
             // Get metadata from the commit at the commit of the full repository
             $comment = escapeshellarg(exec("git -C $fullRepository log -1 $commit --pretty=\"%s\""));
             $commit_date = escapeshellarg(exec("git -C $fullRepository log -1 $commit --pretty=\"%at\""));
-            $author_name = exec("git -C $fullRepository log -1 $commit --pretty=\"%an\"");
-            $author_email = exec("git -C $fullRepository log -1 $commit --pretty=\"%ae\"");
+            $author_name = escapeshellarg(exec("git -C $fullRepository log -1 $commit --pretty=\"%an\""));
+            $author_email = escapeshellarg(exec("git -C $fullRepository log -1 $commit --pretty=\"%ae\""));
             exec("git -C user.email $author_email");
             exec("git -C user.name $author_name");
             print("Author email: $author_email\n");
-            print("$author_name \<$author_email\>");
-            print(escapeshellarg("$author_name \<$author_email\>"));
-            print("$author_name <$author_email>");
-            $author = "$author_name \<$author_email\>";
         
             print "Comment is $comment and author is '$author' and date is $commit_date\n";
             passthru("git -C $canonicalRepository status");
-            print("git -C $canonicalRepository commit --message=$comment --author=$author --date=$commit_date 2>&1");
-            passthru("git -C $canonicalRepository commit --message=$comment --author=$author --date=$commit_date 2>&1", $commitStatus);
+            print("git -C $canonicalRepository commit --message=$comment --date=$commit_date 2>&1");
+            passthru("git -C $canonicalRepository commit --message=$comment --date=$commit_date 2>&1", $commitStatus);
             print("Commit done: $commitStatus\n");
             if ($commitStatus != 0) {
                 break;
