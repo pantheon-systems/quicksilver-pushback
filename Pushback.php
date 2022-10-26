15ab64b71f307b8f3b8bb71a35cb60591f4b69be
Kevin Porras <kevin.porras@pantheon.io>
2022-10-26 16:24:45 -0600

    Cleanup.

--- Pushback.php
+++ Pushback.php
@@ -84,7 +84,7 @@ class Pushback {
                 break;
         }
 
-        $status = $this->doPushback2($fullRepository, $workDir, $upstreamRepoWithCredentials, $buildMetadata, "build-metadata.json");
+        $status = $this->doPushback($fullRepository, $workDir, $upstreamRepoWithCredentials, $buildMetadata, "build-metadata.json");
 
         // Throw out the working repository.
         passthru("rm -rf $workDir");
@@ -178,7 +178,7 @@ class Pushback {
     /**
      * Do the actual push back to the source repo.
      */
-    private function doPushback2($fullRepository, $workDir, $upstreamRepoWithCredentials, $buildMetadata, $buildMetadataFile)
+    private function doPushback($fullRepository, $workDir, $upstreamRepoWithCredentials, $buildMetadata, $buildMetadataFile)
     {
         print "::::::::::::::::: Build Metadata :::::::::::::::::\n";
         var_export($buildMetadata);
@@ -197,10 +197,8 @@ class Pushback {
         $commitToSubmit = exec("git -C $fullRepository rev-parse HEAD");
 
         exec("git -C $fullRepository log $fromSha --pretty=format:%H", $output, $status);
-        print("Status: $status\n");
         if ($status === 0) {
             // We will cherry-pick everything from $fromSha to $commitToSubmit excluding $commitWithBuildMetadataFile.
-            print("git -C $fullRepository rev-list --ancestry-path $fromSha..$commitToSubmit");
             exec("git -C $fullRepository rev-list --ancestry-path $fromSha..$commitToSubmit", $commits);
         } else {
             // fromSha does not exist here, use all of the available commits.
@@ -222,7 +220,6 @@ class Pushback {
         // Make a working clone of the Git branch. Clone just the branch
         // and commit we need.
         passthru("git clone $upstreamRepoWithCredentials --branch $branch $canonicalRepository 2>&1");
-        print("Cloning done.\n");
 
         // If there have been extra commits, then unshallow the repository so that
         // we can make a branch off of the commit this multidev was built from.
@@ -231,7 +228,6 @@ class Pushback {
 
         // A working branch to make changes on
         $targetBranch = $branch;
-        print("Checkpoint 1\n");
 
         // If there are conflicting commits, or if this new commit is on the master
         // branch, then we will work from and push to a branch with a different name.
@@ -244,12 +240,8 @@ class Pushback {
         elseif ($remoteHead != $fromSha) {
             $createNewBranchReason = "new conflicting commits (e.g. $remoteHead) were added to the upstream repository";
         }
-        print("Checkpoint 2\n");
         passthru("git -C $canonicalRepository remote add pantheon $fullRepository 2>&1");
-        print("Checkpoint 3\n");
         passthru("git -C $canonicalRepository fetch pantheon 2>&1");
-        print("Checkpoint 4\n");
-        print("Adding remote done.\n");
         if (!empty($createNewBranchReason)) {
             // Warn that a new branch is being created.
             $targetBranch = substr($commitToSubmit, 0, 5) . $branch;
@@ -257,7 +249,6 @@ class Pushback {
         }
         $localBranchName = "canon-$targetBranch";
         passthru("git -C $canonicalRepository checkout -b $localBranchName 2>&1");
-        print("New branch done.\n");
 
         foreach ($commits as $commit) {
             if ($commit == $commitWithBuildMetadataFile) {
@@ -269,7 +260,6 @@ class Pushback {
             if ($cherryPickResult != '') {
                 $this->raiseDashboardError("Cherry-pick failed with message: $cherryPickResult");
             }
-            print("Cherry-pick done.\n");
 
             // Get metadata from the commit at the commit of the full repository
             $comment = escapeshellarg(exec("git -C $fullRepository log -1 $commit --pretty=\"%s\""));
@@ -281,9 +271,7 @@ class Pushback {
         
             print "Comment is $comment and date is $commit_date\n";
             passthru("git -C $canonicalRepository status");
-            print("git -C $canonicalRepository commit --message=$comment --date=$commit_date 2>&1");
             passthru("git -C $canonicalRepository commit --message=$comment --date=$commit_date 2>&1", $commitStatus);
-            print("Commit done: $commitStatus\n");
             if ($commitStatus != 0) {
                 break;
             }
@@ -293,7 +281,6 @@ class Pushback {
     
             // Push the new branch back to Pantheon
             passthru("git -C $canonicalRepository push origin $localBranchName:$targetBranch 2>&1");
-            print("Push done.\n");
     
             // TODO: If a new branch was created, it would be cool to use the Git API
             // to create a new PR. If there is an existing PR (i.e. branch not master),
@@ -303,167 +290,6 @@ class Pushback {
     
         return $commitStatus;
 
-    }
-
-    /**
-     * Do the actual push back to the source repo.
-     */
-    private function doPushback($fullRepository, $workDir, $upstreamRepoWithCredentials, $buildMetadata, $buildMetadataFile)
-    {
-        print "::::::::::::::::: Build Metadata :::::::::::::::::\n";
-        var_export($buildMetadata);
-        print "\n\n";
-
-        // The last commit made on the lean repo prior to creating the build artifacts
-        $fromSha = $buildMetadata['sha'];
-    
-        // The name of the PR branch
-        $branch = $buildMetadata['ref'];
-        // When working from HEAD, use branch master.
-        if ($branch == 'HEAD') {
-            $branch = 'master';
-        }
-    
-        // The commit to cherry-pick
-        $commitToSubmit = exec("git -C $fullRepository rev-parse HEAD");
-    
-        // Seatbelts: is build metadatafile modified in the HEAD commit?
-        $commitWithBuildMetadataFile = exec("git -C $fullRepository log -n 1 --pretty=format:%H -- $buildMetadataFile");
-        if ($commitWithBuildMetadataFile == $commitToSubmit) {
-            print "Ignoring commit because it contains build assets.\n";
-            return;
-        }
-    
-        // A working branch to make changes on
-        $targetBranch = $branch;
-    
-        print "::::::::::::::::: Info :::::::::::::::::\n";
-        print "We are going to check out $branch from {$buildMetadata['url']}, branch from $fromSha and cherry-pick to $commitToSubmit onto it\n";
-    
-        $canonicalRepository = "$workDir/scratchRepository";
-        $workbranch = "recommit-work";
-    
-        // Make a working clone of the Git branch. Clone just the branch
-        // and commit we need.
-        passthru("git clone $upstreamRepoWithCredentials --depth=1 --branch $branch --single-branch $canonicalRepository 2>&1");
-    
-        // If there have been extra commits, then unshallow the repository so that
-        // we can make a branch off of the commit this multidev was built from.
-        print "git rev-parse HEAD\n";
-        $remoteHead = exec("git -C $canonicalRepository rev-parse HEAD");
-        if ($remoteHead != $fromSha) {
-            // TODO: If we had git 2.11.0, we could use --shallow-since with the date
-            // from $buildMetadata['commit-date'] to get exactly the commits we need.
-            // Until then, though, we will just `unshallow` the whole branch if there
-            // is a conflicting commit.
-            print "git fetch --unshallow\n";
-            passthru("git -C $canonicalRepository fetch --unshallow 2>&1");
-        }
-    
-        // Get metadata from the commit at the HEAD of the full repository
-        $comment = escapeshellarg(exec("git -C $fullRepository log -1 --pretty=\"%s\""));
-        $commit_date = escapeshellarg(exec("git -C $fullRepository log -1 --pretty=\"%at\""));
-        $author_name = exec("git -C $fullRepository log -1 --pretty=\"%an\"");
-        $author_email = exec("git -C $fullRepository log -1 --pretty=\"%ae\"");
-        $author = escapeshellarg("$author_name <$author_email>");
-    
-        print "Comment is $comment and author is $author and date is $commit_date\n";
-        // Make a safe space to store stuff
-        $safe_space = "$workDir/safe-space";
-        mkdir($safe_space);
-    
-        // If there are conflicting commits, or if this new commit is on the master
-        // branch, then we will work from and push to a branch with a different name.
-        // The user should then create a new PR, and use the Git Provider UI to resolve
-        // any conflicts (or clone the branch locally to do the same thing).
-        $createNewBranchReason = '';
-        if ($branch == 'master') {
-            $createNewBranchReason = "the $branch branch cannot be pushed to directly";
-        }
-        elseif ($remoteHead != $fromSha) {
-            $createNewBranchReason = "new conflicting commits (e.g. $remoteHead) were added to the upstream repository";
-        }
-        if (!empty($createNewBranchReason)) {
-            // Warn that a new branch is being created.
-            $targetBranch = substr($commitToSubmit, 0, 5) . $branch;
-            print "Creating a new branch, '$targetBranch', because $createNewBranchReason.\n";
-            print "git checkout -B $targetBranch $fromSha\n";
-            passthru("git -C $canonicalRepository checkout -B $targetBranch $fromSha 2>&1");
-        }
-    
-        // Now for some git magic.
-        //
-        // - $fullRepository contains all of the files we want to commit (and more).
-        // - $canonicalRepository is where we want to commit them.
-        //
-        // The .gitignore file in the canonical repository is correctly configured
-        // to ignore the build results that we do not want from the full repository.
-        //
-        // To affect the change, we will:
-        //
-        // - Copy the .gitignore file from the canonical repository to the full repo.
-        // - Operate on the CONTENTS of the full repository with the .git directory
-        //   of the canonical repository via the --git-dir and -C flags.
-        // - We restore the .gitignore at the end via `git checkout -- .gitignore`.
-    
-        $gitignore_contents = file_get_contents("$canonicalRepository/.gitignore");
-        file_put_contents("$fullRepository/.gitignore", $gitignore_contents);
-    
-        print "::::::::::::::::: .gitignore :::::::::::::::::\n$gitignore_contents\n";
-    
-        // Add our files and make our commit
-        print "git add .\n";
-        passthru("git --git-dir=$canonicalRepository/.git -C $fullRepository add .", $status);
-        if ($status != 0) {
-            print "FAILED with $status\n";
-        }
-        // We don't want to commit the build-metadata to the canonical repository.
-        passthru("git --git-dir=$canonicalRepository/.git -C $fullRepository reset HEAD $buildMetadataFile");
-        // TODO: Copy author, message and perhaps other attributes from the commit at the head of the full repository
-        passthru("git --git-dir=$canonicalRepository/.git -C $fullRepository commit -q --no-edit --message=$comment --author=$author --date=$commit_date", $commitStatus);
-    
-        // Get our .gitignore back
-        passthru("git -C $fullRepository checkout -- .gitignore");
-    
-        // Make sure that HEAD changed after 'git apply'
-        $appliedCommit = exec("git -C $canonicalRepository rev-parse HEAD");
-    
-        // Seatbelts: this generally should not happen. If it does, we will presume
-        // it is not an error; this situation might arise if someone commits only
-        // changes to build result files from dashboard.
-        if ($appliedCommit == $remoteHead) {
-            print "'git commit' did not add a commits. Status code: $commitStatus\n";
-            return;
-        }
-    
-        exec("git -C $canonicalRepository diff-tree --no-commit-id --name-only -r HEAD", $committedFiles);
-        $committedFiles = implode("\n", $committedFiles);
-        if (empty($committedFiles)) {
-            print "Commit $appliedCommit does not contain any files.\n";
-            return;
-        }
-        // Even more seatbelts: ensure that there is nothing in the
-        // commit that should not have been modified. Our .gitignore
-        // file should ensure this never happens. For now, only test
-        // 'vendor'.
-        if (preg_match('#^vendor/#', $committedFiles)) {
-            print "Aborting: commit $appliedCommit contains changes to the 'vendor' directory.\n";
-            return 1;
-        }
-    
-        // If the apply worked, then push the commit back to the light repository.
-        if (($commitStatus == 0) && ($appliedCommit != $remoteHead)) {
-    
-            // Push the new branch back to Pantheon
-            passthru("git -C $canonicalRepository push $upstreamRepoWithCredentials $targetBranch 2>&1");
-    
-            // TODO: If a new branch was created, it would be cool to use the Git API
-            // to create a new PR. If there is an existing PR (i.e. branch not master),
-            // it would also be cool to cross-reference the new PR to the old PR. The trouble
-            // here is converting the branch name to a PR number.
-        }
-    
-        return $commitStatus;
     }
 
 }
