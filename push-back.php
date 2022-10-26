6c77b22ff9750eda34004650539f8735d2f09b83
Kevin Porras <kevin.porras@pantheon.io>
2022-10-26 12:10:57 -0600

    Reorder includes.

--- push-back.php
+++ push-back.php
@@ -3,8 +3,8 @@
 include __DIR__ . '/http-build-url.php';
 
 include __DIR__ . '/Pushback.php';
-include __DIR__ . '/SecretsBase.php';
 include __DIR__ . '/SecretsInterface.php';
+include __DIR__ . '/SecretsBase.php';
 include __DIR__ . '/SecretsLegacy.php';
 
 
